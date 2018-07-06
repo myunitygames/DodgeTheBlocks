cd0bcaf76556419d3bb1abfc2fa5c0b2bbe31ceb
Zöld Krisztián <krisztiangreen@gmail.com>
2018-07-06 14:18:54 +0200

    First Update

---
+++ DodgeTheBlocks/Assets/Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    public float gravityScaleAdjust = 20f;
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / gravityScaleAdjust;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //destroy the fallen blocks
+		if(transform.position.y < -2f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
